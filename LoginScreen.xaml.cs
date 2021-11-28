b91a31a991ea17b70b30610c416f14abae84a8cd
Pavithra-N2001 <pavinsandi@gmail.com>
2021-11-28 14:06:30 +0530

    Add project files.

---
+++ LoginScreen.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LoginDesktopApp
+{
+    /// <summary>
+    /// Interaction logic for LoginScreen.xaml
+    /// </summary>
+    public partial class LoginScreen : Window
+    {
+        public LoginScreen()
+        {
+            InitializeComponent();
+        }
+        MainWindow welcome = new MainWindow();
+
+        private void btnSubmit_Click(object sender, RoutedEventArgs e)
+        {
+
+            string name = txtUsername.Text;
+            string password = txtPassword.Password;
+            SqlConnection sqlCon = new SqlConnection(@"Data Source=LAPTOP-DROLOIQS;Initial Catalog=LoginDB;Integrated Security=True");
+            sqlCon.Open();
+            SqlCommand cmd = new SqlCommand("Select * from tblUser where username='" + name + "' and password='" + password + "'", sqlCon);
+            cmd.CommandType = CommandType.Text;
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            adapter.SelectCommand = cmd;
+            DataSet dataSet = new DataSet();
+            adapter.Fill(dataSet);
+            if (txtUsername.Text.Length == 0)
+            {
+                errormessage.Text = "Please enter username.";
+            }
+            if (dataSet.Tables[0].Rows.Count > 0)
+            {
+                string username = dataSet.Tables[0].Rows[0]["username"].ToString() + " ";
+                welcome.TextBlockName.Text = username;//Sending value from one form to another form.  
+                welcome.Show();
+                Close();
+            }
+            else
+            {
+                errormessage.Text = "Sorry! Please enter existing username/password.";
+            }
+            sqlCon.Close();
+
+
+        }
+
+        private void Register_Click(object sender, RoutedEventArgs e)
+        {
+            Register reg = new Register();
+            reg.Show();
+
+        }
+    
+}
+}
---
+++ Register.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LoginDesktopApp
+{
+    /// <summary>
+    /// Interaction logic for Register.xaml
+    /// </summary>
+    public partial class Register : Window
+    {
+        public Register()
+        {
+            InitializeComponent();
+        }
+        private void reg_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtUsername.Text.Length == 0)
+            {
+                errormessage.Text = "Enter a Username.";
+            }
+            else if (txtPassword.Password != repPassword.Password)
+            {
+                errormessage.Text = "Confirm password must be same as password.";
+            }
+            else
+            {
+                String name = txtUsername.Text;
+                String passWord = txtPassword.Password;
+                String repPass = repPassword.Password;
+
+                SqlConnection sqlCon = new SqlConnection(@"Data Source=LAPTOP-DROLOIQS;Initial Catalog=LoginDB;Integrated Security=True");
+                sqlCon.Open();
+
+                SqlCommand cmd = new SqlCommand("Select * from tblUser where username='" + name + "'", sqlCon);
+                cmd.CommandType = CommandType.Text;
+                SqlDataAdapter adapter = new SqlDataAdapter();
+                adapter.SelectCommand = cmd;
+                DataSet dataSet = new DataSet();
+                adapter.Fill(dataSet);
+                if (dataSet.Tables[0].Rows.Count == 0)
+                {
+                    SqlCommand cm = new SqlCommand("Insert into tblUser (UserName,Password) values('" + name + "','" + passWord + "')", sqlCon);
+                    cm.CommandType = CommandType.Text;
+                    cm.ExecuteNonQuery();
+                    sqlCon.Close();
+                    errormessage.Text = "You have Registered successfully.";
+                }
+                else
+                {
+                    errormessage.Text = "User name Already Exists.";
+                }
+
+            }
+
+        }
+
+
+    }
+}
